ec69000d78a5fc99f05fb09b35c75884c517da10
Nico Lucciola <nico.lucciola@gmail.com>
2018-06-16 11:37:47 +0200

    #12999

--- src/qt/machinecoingui.cpp
+++ src/qt/machinecoingui.cpp
@@ -1032,6 +1032,11 @@ void MachinecoinGUI::changeEvent(QEvent *e)
                 QTimer::singleShot(0, this, SLOT(hide()));
                 e->ignore();
             }
+            else if((wsevt->oldState() & Qt::WindowMinimized) && !isMinimized())
+            {
+                QTimer::singleShot(0, this, SLOT(show()));
+                e->ignore();
+            }
         }
     }
 #endif
