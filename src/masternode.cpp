d766efdb0d521688db7da9ce2308d82ce637c680
Nico Lucciola <nico.lucciola@gmail.com>
2018-06-18 12:13:54 +0200

    mn: use for instead of BOOST_FOREACH

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -316,7 +316,7 @@ void CMasternode::UpdateLastPaid(const CBlockIndex *pindex, int nMaxBlocksToScan
     CScript mnpayee = GetScriptForDestination(CScriptID(GetScriptForDestination(WitnessV0KeyHash(pubKeyCollateralAddress.GetID()))));
     LogPrint(MCLog::MN, "CMasternode::UpdateLastPaidBlock -- searching for block with payment to %s\n", vin.prevout.ToStringShort());
 
-    LOCK(cs_mapMasternodeBlocks);
+    //LOCK(cs_mapMasternodeBlocks);
 
     for (int i = 0; pindexActive->nHeight > nBlockLastPaid && i < nMaxBlocksToScanBack; i++) {
         if(mnpayments.mapMasternodeBlocks.count(pindexActive->nHeight) &&
@@ -331,7 +331,7 @@ void CMasternode::UpdateLastPaid(const CBlockIndex *pindex, int nMaxBlocksToScan
             if(ret == true) {
                 CAmount nMasternodePayment = GetMasternodePayment(pindexActive->nHeight, block.vtx[0]->GetValueOut());
 
-                BOOST_FOREACH(CTxOut txout, block.vtx[0]->vout) {
+                for (CTxOut txout : block.vtx[0]->vout) {
                     if(mnpayee == txout.scriptPubKey && nMasternodePayment == txout.nValue) {
                         nBlockLastPaid = pindexActive->nHeight;
                         nTimeLastPaid = pindexActive->nTime;
