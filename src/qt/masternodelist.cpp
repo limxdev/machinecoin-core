835ddc0857017b1dc2ba39b2644d0652becf406c
Nico Lucciola <nico.lucciola@gmail.com>
2018-07-13 15:39:44 +0200

    add missing QT patch filtered by gitignore

--- src/qt/masternodelist.cpp
+++ src/qt/masternodelist.cpp
@@ -117,7 +117,7 @@ void MasternodeList::StartAlias(std::string strAlias)
             bool fSuccess = CMasternodeBroadcast::Create(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
 
             int nDoS;
-            if (fSuccess && !mnodeman.CheckMnbAndUpdateMasternodeList(NULL, mnb, nDoS, *g_connman)) {
+            if (fSuccess && !mnodeman.CheckMnbAndUpdateMasternodeList(NULL, mnb, nDoS, g_connman.release())) {
                 strError = "Failed to verify MNB";
                 fSuccess = false;
             }
@@ -164,7 +164,7 @@ void MasternodeList::StartAll(std::string strCommand)
         bool fSuccess = CMasternodeBroadcast::Create(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
 
         int nDoS;
-        if (fSuccess && !mnodeman.CheckMnbAndUpdateMasternodeList(NULL, mnb, nDoS, *g_connman)) {
+        if (fSuccess && !mnodeman.CheckMnbAndUpdateMasternodeList(NULL, mnb, nDoS, g_connman.release())) {
             strError = "Failed to verify MNB";
             fSuccess = false;
         }
