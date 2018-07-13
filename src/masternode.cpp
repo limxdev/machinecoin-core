c43e110a4c3ddf725051fbd277921b390a061a39
Nico Lucciola <nico.lucciola@gmail.com>
2018-07-13 13:34:13 +0200

    ---

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -116,7 +116,7 @@ CMasternode::CollateralStatus CMasternode::CheckCollateral(const COutPoint& outp
         return COLLATERAL_INVALID_AMOUNT;
     }
 
-    if(pubkey == CPubKey() || coin.out.scriptPubKey != GetScriptForDestination(WitnessV0KeyHash(pubkey.GetID()))) {
+    if(pubkey == CPubKey() || coin.out.scriptPubKey != GetScriptForDestination(CScriptID(GetScriptForDestination(WitnessV0KeyHash(pubkey.GetID()))))) {
         return COLLATERAL_INVALID_PUBKEY;
     }
 
